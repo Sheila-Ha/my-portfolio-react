01564ad6d41f5374d7367325b8ed4be65583ed89
Sheila Hanson <slhanson11@live.com>
2024-03-26 21:23:55 -0500

    moved resume img to utils

--- src/assets/pages/Resume.jsx
+++ src/assets/pages/Resume.jsx
@@ -1,7 +1,22 @@
+import React from 'react';
+import resume from '../utils/Resume.pdf'
+import { Document, Page, pdfjs } from 'react-pdf'
+pdfjs.GlobalWorkerOptions.workSrc = '//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js'
+
 export default function Resume() {
     return (
         <div>
-          <h1>[Resume](public/images/Sheila Hanson Resume 2024.pdf) </h1>
+          <h1>Resume</h1>
         </div>
-    );
-  }
+    ),
+        <div className='res'>
+          <h2 className='heading'>Resume</h2>
+            <Document
+              file={resume}
+              onLoadError={console.error}
+              style={{ width: '100vw', height: 'auto'}}
+            >
+              <Page pageIndex={0}/>
+            </Document>
+        </div>
+}
