5d541811f3a0cd180700f4f26ab403f4257e6e5c
Sheila Hanson <slhanson11@live.com>
2024-03-27 23:50:45 -0500

    updated navbar name

--- src/assets/components/NavBar.jsx
+++ src/assets/components/NavBar.jsx
@@ -25,12 +25,12 @@ function NavBar() {
         <li className="nav-item">
           <Link
             to="/AboutMe"
-            // Check to see if the currentPage is `About Me`, and if so we use the active link class from bootstrap. Otherwise, we set it to a normal nav-link
+            // Check to see if the currentPage is `AboutMe`, and if so we use the active link class from bootstrap. Otherwise, we set it to a normal nav-link
             className={
               currentPage === "/AboutMe" ? "nav-link active" : "nav-link text-black"
             }
           >
-            About
+            About Me
           </Link>
         </li>
         <li className="nav-item">
