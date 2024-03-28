58d3f32cda2cb48f725cb80786b89ceed2ad4da4
Sheila Hanson <slhanson11@live.com>
2024-03-27 23:14:24 -0500

    Updated portfolio image paths

--- src/assets/pages/Portfolio.jsx
+++ src/assets/pages/Portfolio.jsx
@@ -11,62 +11,62 @@ function Portfolio() {
           <div className="flex flex-wrap justify-center gap-2 text-white">
           <PortfolioCard 
           link="https://github.com/Sheila-Ha/5-day-forecast.git/"
-          image="public/images/weather.png"
+          image="images/weather.png"
           >
           </PortfolioCard >
           <PortfolioCard 
           link="https://github.com/Sheila-Ha/food-and-recipe/"
-          image="public/images/food_recipes.png"
+          image="images/food_recipes.png"
           >
           </PortfolioCard >
           <PortfolioCard 
           link="https://github.com/Sheila-Ha/orm-ecom-back-end.git"
-          image="public/images/ORM.png"
+          image="images/ORM.png"
           >
           </PortfolioCard >
           <PortfolioCard 
           link="https://github.com/Sheila-Ha/mvc-tech-blog.git"
-          image="public/images/MVC.png"
+          image="images/MVC.png"
           >
           </PortfolioCard >
           <PortfolioCard 
           link="https://github.com/Sheila-Ha/code-quiz.git"
-          image="public/images/code_quiz.png"
+          image="images/code_quiz.png"
           >
           </PortfolioCard >
           <PortfolioCard 
           link="https://github.com/Sheila-Ha/scheduler.git"
-          image="public/images/scheduler.png"
+          image="images/scheduler.png"
           >
           </PortfolioCard >
           <PortfolioCard 
           link="https://github.com/Sheila-Ha/Portfolio.git"
-          image="public/images/1stPortfolio.png"
+          image="images/1stPortfolio.png"
           >
           </PortfolioCard >
           <PortfolioCard 
           link="https://github.com/Sheila-Ha/sql-tracker.git"
-          image="public/images/sql_tracker.png"
+          image="images/sql_tracker.png"
           >
           </PortfolioCard >
           <PortfolioCard 
           link="https://github.com/Sheila-Ha/Group4-Poll-Meet-Up.git"
-          image="public/images/pollcreated.png"
+          image="images/pollcreated.png"
           >
           </PortfolioCard >
           <PortfolioCard 
           link="https://github.com/Sheila-Ha/Social-Network-API.git"
-          image="public/images/SN_API.png"
+          image="images/SN_API.png"
           >
           </PortfolioCard >
           <PortfolioCard 
           link="https://github.com/Sheila-Ha/note-taker.git"
-          image="public/images/note_taker.png"
+          image="images/note_taker.png"
           >
           </PortfolioCard >
           <PortfolioCard 
           link="https://github.com/Sheila-Ha/my-portfolio-react.git"
-          image="public/images/Portfolio_home.png"
+          image="images/Portfolio_home.png"
           >
           </PortfolioCard >
         </div>
